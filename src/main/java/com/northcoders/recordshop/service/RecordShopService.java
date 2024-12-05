2b5c209d9fe3ea8ed5acffc91d7e2216f8455b2e
Nesrin Kurt <kurtnesrin90@yahoo.com>
2024-12-05 13:28:12 +0000

    feat: create RecordShopService interface and RecordShopService class for business layer

---
+++ src/main/java/com/northcoders/recordshop/service/RecordShopService.java
@@ -0,0 +1,11 @@
+package com.northcoders.recordshop.service;
+
+import com.northcoders.recordshop.model.Album;
+import java.util.List;
+
+public interface RecordShopService {
+
+    // This interface will have the method signatures
+
+    List<Album> getAllAlbums();
+}
---
+++ src/main/java/com/northcoders/recordshop/service/RecordShopServiceImpl.java
@@ -0,0 +1,27 @@
+package com.northcoders.recordshop.service;
+
+import com.northcoders.recordshop.model.Album;
+import com.northcoders.recordshop.repository.RecordShopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//This class will have the implementations for the methods
+
+@Service
+public class RecordShopServiceImpl implements RecordShopService {
+
+    @Autowired
+    RecordShopRepository recordShopRepository;
+
+    @Override
+    public List<Album> getAllAlbums() {
+        List<Album> albums = new ArrayList<>();
+        recordShopRepository.findAll().forEach(albums::add);
+        return albums;
+    }
+
+
+}
