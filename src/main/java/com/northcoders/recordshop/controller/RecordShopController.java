29dd70453cdfab365ebd73ddc049f0c344b947ec
Nesrin Kurt <kurtnesrin90@yahoo.com>
2024-12-06 11:46:46 +0000

    feat: add GetMapping request using with @PathVariable _id to get the album by its id

--- src/main/java/com/northcoders/recordshop/controller/RecordShopController.java
+++ src/main/java/com/northcoders/recordshop/controller/RecordShopController.java
@@ -5,25 +5,32 @@ import com.northcoders.recordshop.service.RecordShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
-@RequestMapping("/api/v1/album")
+@RequestMapping("/api/v1/albums")
 public class RecordShopController {
 
     @Autowired
     RecordShopService recordShopService;
 
-    @GetMapping
+    @GetMapping()
     public ResponseEntity<List<Album>> getAllAlbums(){
         List<Album> albums = recordShopService.getAllAlbums();
-        return new ResponseEntity<>(albums, HttpStatus.OK);
+        return new ResponseEntity<>(albums , HttpStatus.OK);
+    }
 
-
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Album>> getAlbumById(@PathVariable Long id){
+        Optional<Album> albumById = recordShopService.getAlbumById(id);
+        if(albumById.isEmpty()){
+            return ResponseEntity.status(404).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(albumById);
+        }
     }
 }
