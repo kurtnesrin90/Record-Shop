a6a102c56c81c7565f9b75701e4e5d2593c25919
Nesrin Kurt <kurtnesrin90@yahoo.com>
2024-12-06 11:43:13 +0000

    feat: create getAlbumById()

--- src/main/java/com/northcoders/recordshop/service/RecordShopService.java
+++ src/main/java/com/northcoders/recordshop/service/RecordShopService.java
@@ -2,10 +2,13 @@ package com.northcoders.recordshop.service;
 
 import com.northcoders.recordshop.model.Album;
 import java.util.List;
+import java.util.Optional;
 
 public interface RecordShopService {
 
     // This interface will have the method signatures
 
     List<Album> getAllAlbums();
+
+    Optional<Album> getAlbumById(Long id);
 }
